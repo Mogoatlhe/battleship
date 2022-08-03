3cdc4e6076aa7e311c32b2de568be9f653a09d93
mogoatlhe <iam@ofent.se>
2022-08-03 13:32:25 +0200

    ensure whole ship is on the grid before making grid a drop target

--- src/view/dragover.js
+++ src/view/dragover.js
@@ -1,19 +1,46 @@
 export default () => {
 	const dragTarget = document.querySelector("#your-fleet .grid-container");
+
 	const getClickedShipPart = (dragging) =>
 		[...dragging.childNodes].findIndex((dragChild) =>
 			dragChild.classList.contains("clicked")
 		);
 
+	const getIsWholeShipInGrid = (length, id, index) => {
+		const startingPos = id - index;
+		const endingPos = startingPos + length;
+
+		if (
+			// horizontal. vertical??????
+			startingPos < 0 ||
+			endingPos > 99 ||
+			String(startingPos).length !== String(endingPos).length ||
+			(String(id).length !== 1 &&
+				String(startingPos)[0] !== String(endingPos)[0])
+		) {
+			return false;
+		}
+		return true;
+	};
+
 	dragTarget.addEventListener("dragover", (e) => {
-		e.preventDefault();
 		const dragging = document.querySelector(".dragging");
-		// const length = dragging.childNodes.length - 1;
-		// const dragChildIndex = getClickedShipPart(dragging);
+		const length = dragging.childNodes.length - 1;
+		const dataID = Number(e.target.getAttribute("data-id"));
+		const dragChildIndex = getClickedShipPart(dragging);
+		const isWholeShipInGrid = getIsWholeShipInGrid(
+			length,
+			dataID,
+			dragChildIndex
+		);
+
+		if (!isWholeShipInGrid) {
+			return;
+		}
+
+		e.preventDefault();
 		// 	const draggingParent = dragging.parentNode;
-		// 	const isGridContainer = draggingParent.classList.contains("grid-container");
-		// 	const dataID = e.target.getAttribute("data-id");
-		// 	if (!isGridContainer && (dataID === null || dataID === "")) {
+		// 	if (dataID === null || dataID === "") {
 		// 		[...dragging.childNodes].forEach((c) => c.removeAttribute("data-id"));
 		// 		return;
 		// 	}
