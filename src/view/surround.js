351d9989ba99dde2710615fdc8e925ad951af307
mogoatlhe <iam@ofent.se>
2022-08-04 16:46:58 +0200

    surround ship diagonally

--- src/view/surround.js
+++ src/view/surround.js
@@ -55,6 +55,50 @@ export default () => {
 		downShipPart.classList.add(`surround-${currentShipPartId}`);
 	};
 
+	const doDiagonalLeftUpSurround = (currentShipPartId) => {
+		if (currentShipPartId.length === 1 || currentShipPartId[1] === "0") {
+			return;
+		}
+
+		const diagonalLeftUpShipPartId = Number(currentShipPartId) - 11;
+		const diagonalLeftUpShipPart = document.querySelector(
+			`[data-id="${diagonalLeftUpShipPartId}"]`
+		);
+		diagonalLeftUpShipPart.classList.add(`surround-${currentShipPartId}`);
+	};
+
+	const doDiagonalRightUpSurround = (currentShipPartId) => {
+		if (currentShipPartId.length === 1 || currentShipPartId[1] === "9") {
+			return;
+		}
+
+		const diagonalRightUpShipPartId = Number(currentShipPartId) - 9;
+		const diagonalRightUpShipPart = document.querySelector(
+			`[data-id="${diagonalRightUpShipPartId}"]`
+		);
+		diagonalRightUpShipPart.classList.add(`surround-${currentShipPartId}`);
+	};
+
+	const doDiagonalLeftDownSurround = (currentShipPartId) => {
+		if (currentShipPartId[0] === "9" || currentShipPartId[1] === "0") {
+			return;
+		}
+
+		const shipPartId = Number(currentShipPartId) + 9;
+		const shipPart = document.querySelector(`[data-id="${shipPartId}"]`);
+		shipPart.classList.add(`surround-${currentShipPartId}`);
+	};
+
+	const doDiagonalRightDownSurround = (currentShipPartId) => {
+		if (currentShipPartId[0] === "9" || currentShipPartId[1] === "9") {
+			return;
+		}
+
+		const shipPartId = Number(currentShipPartId) + 11;
+		const shipPart = document.querySelector(`[data-id="${shipPartId}"]`);
+		shipPart.classList.add(`surround-${currentShipPartId}`);
+	};
+
 	const surroundShip = (ship) => {
 		const shipParts = ship.childNodes;
 		const { length } = shipParts;
@@ -64,12 +108,18 @@ export default () => {
 			// horizontal
 			if (index === 0) {
 				doLeftSurround(shipPartId);
+				doDiagonalLeftUpSurround(shipPartId);
+				doDiagonalLeftDownSurround(shipPartId);
 
 				if (length === 1) {
 					doRightSurround(shipPartId);
+					doDiagonalRightUpSurround(shipPartId);
+					doDiagonalRightDownSurround(shipPartId);
 				}
 			} else if (length - 1 === index) {
 				doRightSurround(shipPartId);
+				doDiagonalRightUpSurround(shipPartId);
+				doDiagonalRightDownSurround(shipPartId);
 			}
 
 			doUpSurround(shipPartId);
