9029d56492dba0c84708285f1ee0e3bf63607e4a
mogoatlhe <iam@ofent.se>
2023-08-04 09:09:33 +0200

    refactor: randomise ship placement

--- src/logic/player.ts
+++ src/logic/player.ts
@@ -2,6 +2,7 @@ import Gameboard from "./gameboard";
 
 export default (name: string) => {
 	const pastAttempts: any[] = [];
+	const placedShips: { coords: Coordinates; direction: string }[] = [];
 	const gameboard = Gameboard();
 	const getName = () => name;
 
@@ -19,12 +20,67 @@ export default (name: string) => {
 			const msg = error.message;
 			if (
 				msg === "start values are out of bounds" ||
-				msg === `"direction" value must be either "horizontal" or "vertical"`
+				msg === `"direction" value must be either "horizontal" or "vertical"` ||
+				msg === "invalid ship position"
 			)
 				return false;
 
 			throw msg;
 		}
+	};
+
+	const randomiseShipPlacement = () => {
+		const invalidCoords: { coords: Coordinates; direction: string }[] = [];
+		const ships = gameboard.getShips();
+		const directions = ["horizontal", "vertical"];
+
+		for (let i = ships.length - 1; i > -1; i--) {
+			let isPlaced = false;
+			do {
+				const [row, col, directionsIndex] = getRandomPlacement();
+				let direction = directions[directionsIndex];
+
+				if (validateCoords(row, col, direction, invalidCoords)) continue;
+
+				isPlaced = placeShip({ row: row, col: col }, i, direction);
+				invalidCoords.push({ coords: { row: row, col: col }, direction });
+
+				if (!isPlaced) {
+					direction = directions[Math.abs(directionsIndex - 1)];
+					isPlaced = placeShip({ row: row, col: col }, i, direction);
+					invalidCoords.push({ coords: { row: row, col: col }, direction });
+				}
+
+				if (isPlaced)
+					placedShips.push({ coords: { row: row, col: col }, direction });
+			} while (!isPlaced);
+		}
+
+		return placedShips;
+	};
+
+	const getRandomPlacement = () => {
+		const row = Math.floor(Math.random() * 10);
+		const col = Math.floor(Math.random() * 10);
+		const directionsIndex = Math.floor(Math.random() * 2);
+
+		return [row, col, directionsIndex];
+	};
+
+	const validateCoords = (
+		row: number,
+		col: number,
+		direction: string,
+		invalidCoords: {
+			coords: Coordinates;
+			direction: string;
+		}[]
+	) => {
+		const invalidCoord = { coords: { row: row, col: col }, direction };
+
+		const invalidCoordsStr = JSON.stringify(invalidCoords);
+		if (invalidCoordsStr.includes(JSON.stringify(invalidCoord))) return true;
+		return false;
 	};
 	// const placeShip = (index: number, starting: number, direction: string) => {
 	// 	if (typeof index !== "number") {
@@ -144,7 +200,7 @@ export default (name: string) => {
 		getName,
 		// receiveAttack,
 		placeShip,
-		// randomiseShipPlacement,
+		randomiseShipPlacement,
 		getShips,
 		attack,
 	};
