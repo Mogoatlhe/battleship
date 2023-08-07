40dbd11c5f474298f2fe0531dba62027b3365177
mogoatlhe <iam@ofent.se>
2023-08-07 18:36:10 +0200

    add grid-container

--- src/view/Fleet/Grid.ts
+++ src/view/Fleet/Grid.ts
@@ -2,15 +2,18 @@ import Label from "./GridLabel/labels";
 
 const Grid = () => {
 	const container = document.createElement("div");
+	const gridContainer = document.createElement("div");
 	const numbersContainer = document.createElement("div");
 	const numCoords = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 	const numbers = Label(numCoords, "number");
 
-	container.setAttribute("id", "numbers-grid-container");
+	container.classList.add("numbers-grid-container");
 	numbersContainer.classList.add("numbers");
+	gridContainer.classList.add("grid-container");
 
 	numbersContainer.append(numbers.getLabels());
 	container.append(numbersContainer);
+	container.append(gridContainer);
 
 	const getContainer = () => container;
 
