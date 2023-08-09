15373a5067af08210df52a28e2d9ebfd30ca7240
mogoatlhe <iam@ofent.se>
2023-08-09 10:56:45 +0200

    add grid containers

--- src/view/Fleet/Grid.ts
+++ src/view/Fleet/Grid.ts
@@ -4,20 +4,23 @@ import Player from "../../logic/player";
 
 const Grid = (player: typeof Player) => {
 	const container = document.createElement("div");
+	const numbersGridContainer = document.createElement("div");
 	const gridContainer = document.createElement("div");
 	const numbersContainer = document.createElement("div");
 	const numCoords = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 	const numbers = Label(numCoords, "number");
 
-	container.classList.add("numbers-grid-container");
+	container.classList.add("port-numbers-grid-container");
+	numbersGridContainer.classList.add("numbers-grid-container");
 	numbersContainer.classList.add("numbers");
 	gridContainer.classList.add("grid-container");
 	const gridItem = GridItem(gridContainer, player);
 	gridItem.appendGridItems();
 
 	numbersContainer.append(numbers.getLabels());
-	container.append(numbersContainer);
-	container.append(gridContainer);
+	numbersGridContainer.append(numbersContainer);
+	numbersGridContainer.append(gridContainer);
+	container.append(numbersGridContainer);
 
 	const getContainer = () => container;
 	const getGridItem = () => gridItem;
