01a9045c2fdb97ba76ccec6cbedfafb20e00d2c5
mogoatlhe <iam@ofent.se>
2023-08-10 12:35:09 +0200

    remove reset btn

--- src/view/boardOptions.ts
+++ src/view/boardOptions.ts
@@ -4,13 +4,13 @@ import Reset from "./reset";
 
 const BoardOptions = (grid: typeof Grid) => {
 	const randomise = Randomise(grid);
-	const reset = Reset();
+	// const reset = Reset();
 
 	const optionsContainer = document.createElement("div");
 	optionsContainer.classList.add("flex");
 	optionsContainer.setAttribute("id", "options-container");
 	optionsContainer.append(randomise.getBtn());
-	optionsContainer.append(reset.getResetBtn());
+	// optionsContainer.append(reset.getResetBtn());
 
 	const getBoardOptions = () => optionsContainer;
 
