231c2ac3b8776b78e95e69fa5b4a2a923c0bc4de
mogoatlhe <iam@ofent.se>
2023-08-11 16:07:50 +0200

    dislay fleet status

--- src/view/Fleet/Fleet.ts
+++ src/view/Fleet/Fleet.ts
@@ -18,6 +18,47 @@ const Fleet = (fleetName: string) => {
 
 	gridWithLabels.classList.add("grid-with-labels");
 
+	const shipTypes = document.createElement("div");
+	shipTypes.classList.add("ship-types", "hidden");
+	for (let i = 0; i < 4; i++) {
+		let length, count;
+		const shipType = document.createElement("div");
+
+		if (i < 1) {
+			length = 4;
+			count = 1;
+		} else if (i < 2) {
+			length = 3;
+			count = 2;
+		} else if (i < 3) {
+			length = 2;
+			count = 3;
+		} else {
+			length = 1;
+			count = 4;
+		}
+
+		shipType.append(...attachShips(count, length));
+		shipType.classList.add("port-line", `ship-type-length-${length}`);
+
+		shipTypes.appendChild(shipType);
+	}
+
+	function attachShips(count: number, length: number) {
+		const ships: Node[] = [];
+		const width = `${10 * length}px`;
+
+		for (let i = 0; i < count; i++) {
+			const ship = document.createElement("div");
+			ship.classList.add("ship");
+			ship.style.width = width;
+			ship.style.height = "10px";
+			ships.push(ship);
+		}
+
+		return ships;
+	}
+
 	fleet.append(header.getFleetHeader());
 	gridWithLabels.append(grid.getContainer());
 	gridWithLabels.querySelector(".grid").prepend(alphabets.getLabels());
@@ -27,6 +68,7 @@ const Fleet = (fleetName: string) => {
 
 	const manageFleet = () => {
 		grid.getGridItem().randomiseShipPlacement();
+		gridWithLabels.append(shipTypes);
 		if (fleetName !== "human") {
 			fleet.classList.add("hidden");
 			return;
--- src/view/play.ts
+++ src/view/play.ts
@@ -3,12 +3,16 @@ import Grid from "./Fleet/Grid";
 
 const ShowComputer = () => {
 	const playBtn = document.createElement("button");
+
 	playBtn.classList.add("play");
 	playBtn.textContent = "Play";
 
 	playBtn.addEventListener("click", showComputer);
 
 	function showComputer(e: MouseEvent) {
+		const shipTypes = document.querySelectorAll(".ship-types");
+		shipTypes[0].classList.remove("hidden");
+		shipTypes[1].classList.remove("hidden");
 		const target = e.target as HTMLButtonElement;
 		const targetParent = target.parentElement;
 		const computer = document.querySelector("#computer");
